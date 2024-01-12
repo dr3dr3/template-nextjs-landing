c0762981b325e4857bbd1267b3e5075f94554b24
Andre Dreyer <1818388+dr3dr3@users.noreply.github.com>
2024-01-12 05:01:24 +0000

    Stuff

--- landing/style/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Inter, Poppins } from 'next/font/google';
-
-export const inter = Inter({ subsets: ['latin'] });
-
-export const display = Poppins({
-  weight: ['400', '600', '700', '900'],
-  subsets: ['latin'],
-});
