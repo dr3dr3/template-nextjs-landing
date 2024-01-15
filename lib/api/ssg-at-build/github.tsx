56d9ee314983c585a2bf590dcb5de9d8e956bcad
Andre Dreyer <1818388+dr3dr3@users.noreply.github.com>
2024-01-15 00:05:17 +0000

    Stuff

--- lib/api/ssg-at-build/github.tsx
+++ lib/api/ssg-at-build/github.tsx
@@ -104,7 +104,7 @@ export async function getMeanTimeToRecover() {
   const ghOwner = process.env.GITHUB_REPOSITORY_OWNER || '';
   const repo = ghRepo.replace(/.*?\//, '');
   const ghUrl = `https://api.github.com/repos/${ghOwner}/${repo}/issues?labels=bug&state=closed&per_page=100`;
-  const res = await fetch(ghUrl);
+  const res = await fetch(ghUrl, { next: { revalidate: 3600 } });
 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
